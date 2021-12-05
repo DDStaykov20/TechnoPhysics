edbc93f58f7101d5dfd318547ce0e3a9a9bea5bc
Zhasmina Mihaleva <zsmihaleva20@codingburgas.bg>
2021-12-06 00:03:03 +0200

    add finished second topic + small changes for the first topic

--- Project/techno_physics/techno_physics/techno_physics.cpp
+++ Project/techno_physics/techno_physics/techno_physics.cpp
@@ -12,7 +12,7 @@ void Menu()
 	cout << "4) Pressure and Pascal's law" << endl;
 }
 
-char firstTopic(char symbol, char answer)
+char firstTopic(char symbol, char answer, char answer2)
 {
 	cout << endl;
 
@@ -87,6 +87,29 @@ char firstTopic(char symbol, char answer)
 
 		if (answer == 'a')
 		{
+			cout << endl;
+			cout << "|" << "Correct answer!" << endl;
+			cout << "|_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ " << endl;
+		}
+
+		cout << endl;
+
+		cout << "|- - - - - - - - - - - - - - - " << endl;
+		cout << "|" << "What is acceleration?" << endl;
+		cout << "|" << "a) every change in the relative position of bodies over time " << endl;
+		cout << "|" << "b) the physical quantity that the body becomes rapidly, which changes the speed of a body" << endl; // correct answer
+		cout << "|" << "c) body, according to which we define the position and movement of other bodies" << endl;
+		cout << "|" << endl;
+		cout << "|" << "Your answer(a, b or c): ";
+
+		while (answer2 != 'b')
+		{
+			cin >> answer2;
+		}
+
+		if (answer2 == 'b')
+		{
+			cout << endl;
 			cout << "|" << "Correct answer!" << endl;
 			cout << "|_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ " << endl;
 		}
@@ -95,7 +118,131 @@ char firstTopic(char symbol, char answer)
 	{
 		return 0;
 	}
-} 
+}
+
+char secondTopic(char symbol, char answer, char answer2, char answer3)
+{
+	cout << endl;
+
+	cout << "\t\t" << "Second Topic - Principles of mechanics" << endl;
+
+	cout << endl;
+	cout << endl;
+
+	cout << "|- - - - - - - - - - - - - - - " << endl;
+	cout << "|" << "\t\t" << "Definitions: " << endl;
+	cout << "|" << "First principle of mechanics (Law of Inertia) -> Every body maintains its state of rest or of rectilinear uniform motion until external influence brings it out of this state" << endl;
+	cout << "|" << "Second principle of mechanics -> The acceleration of the body is directly proportional to the force of the forces applied to the body and is inversely proportional to the mass of the body" << endl;
+	cout << "|" << "Third principle of mechanics -> If one body acts on another body with force, then always the second body opposes the second with equal in size and opposite in direction force (Each action has equal in size and opposite in direction counteraction)" << endl;
+	cout << "|_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _" << endl;
+
+	cout << endl;
+	cout << endl;
+
+	cout << "|- - - - - - - - - - - - - " << endl;
+	cout << "|" << "\t\t" << "Formulas: " << endl;
+	cout << "|" << "m (mass) = F / a [kg]" << endl;
+	cout << "|" << "F (force) = m.a [N]" << endl;
+	cout << "|" << "a (acceleration) = F / m [m/s2]" << endl;
+	cout << "|_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _" << endl;
+
+	cout << endl;
+	cout << endl;
+
+	cout << "|- - - - - - - - - - - - - - - - - - - " << endl;
+	cout << "|" << "\t\t" << "Task with solution:" << endl;
+	cout << "|" << "Task:" << endl;
+	cout << "|" << "A body with mass m = 2kg moves under the action of the force F = 10N. How much is the acceleration of the body?" << endl;
+	cout << "|" << endl;
+	cout << "|" << "Solution:" << endl;
+	cout << "|" << "a) a = F / m" << endl;
+	cout << "|" << "a = 10 / 2" << endl;
+	cout << "|" << "a = 5 m/s2" << endl;
+	cout << "|_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _" << endl;
+
+	cout << endl;
+
+	system("pause");
+
+	cout << endl;
+
+	cout << "Do you want to practise your knowledge about principles of mechanics with questions? (Y/N)" << endl;
+
+	while (symbol != 'Y' and symbol != 'N')
+	{
+		cin >> symbol;
+	}
+
+	cout << endl;
+
+	if (symbol == 'Y')
+	{
+		cout << "|- - - - - - - - - - - - - - - " << endl;
+		cout << "|" << "What is the first principle of mechanics?" << endl;
+		cout << "|" << "a) The acceleration of the body is directly proportional to the force of the forces applied to the body and is inversely proportional to the mass of the body" << endl; 
+		cout << "|" << "b) Every body maintains its state of rest or of rectilinear uniform motion until external influence brings it out of this state" << endl; // correct answer
+		cout << "|" << "c) If one body acts on another body with force, then always the second body opposes the second with equal in size and opposite in direction force (Each action has equal in size and opposite in direction counteraction)" << endl;
+		cout << "|" << endl;
+		cout << "|" << "Your answer(a, b or c): ";
+
+		while (answer != 'b')
+		{
+			cin >> answer;
+		}
+
+		if (answer == 'b')
+		{
+			cout << "|" << "Correct answer!" << endl;
+			cout << "|_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ " << endl;
+		}
+
+		cout << endl;
+
+		cout << "|- - - - - - - - - - - - - - - " << endl;
+		cout << "|" << "What is the second principle of mechanics?" << endl;
+		cout << "|" << "a) The acceleration of the body is directly proportional to the force of the forces applied to the body and is inversely proportional to the mass of the body" << endl; // correct answer
+		cout << "|" << "b) Every body maintains its state of rest or of rectilinear uniform motion until external influence brings it out of this state" << endl; 
+		cout << "|" << "c) If one body acts on another body with force, then always the second body opposes the second with equal in size and opposite in direction force (Each action has equal in size and opposite in direction counteraction)" << endl;
+		cout << "|" << endl;
+		cout << "|" << "Your answer(a, b or c): ";
+
+		while (answer2 != 'a')
+		{
+			cin >> answer2;
+		}
+
+		if (answer2 == 'a')
+		{
+			cout << "|" << "Correct answer!" << endl;
+			cout << "|_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ " << endl;
+		}
+
+		cout << endl;
+
+		cout << "|- - - - - - - - - - - - - - - " << endl;
+		cout << "|" << "What is the third principle of mechanics?" << endl;
+		cout << "|" << "a) The acceleration of the body is directly proportional to the force of the forces applied to the body and is inversely proportional to the mass of the body" << endl; 
+		cout << "|" << "b) Every body maintains its state of rest or of rectilinear uniform motion until external influence brings it out of this state" << endl;
+		cout << "|" << "c) If one body acts on another body with force, then always the second body opposes the second with equal in size and opposite in direction force (Each action has equal in size and opposite in direction counteraction)" << endl; // correct answer
+		cout << "|" << endl;
+		cout << "|" << "Your answer(a, b or c): ";
+
+		while (answer3 != 'c')
+		{
+			cin >> answer3;
+		}
+
+		if (answer3 == 'c')
+		{
+			cout << "|" << "Correct answer!" << endl;
+			cout << "|_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ " << endl;
+		}
+	}
+	else
+	{
+		return 0;
+	}
+}
 
 int main()
 {
@@ -103,6 +250,8 @@ int main()
 	bool flag = false;
 	char symbolTopics= 'f';
 	char answers = 'f';
+	char answersTwo = 'f';
+	char answersThree = 'f';
 
 	cout << "Hello!" << endl;
 	cout << "Do you want to play? (Yes/No)" << endl;
@@ -139,7 +288,35 @@ int main()
 		{
 		case 1:
 
-			firstTopic(symbolTopics, answers);
+			firstTopic(symbolTopics, answers, answersTwo);
+
+			cout << endl;
+
+			cout << "Do you want to continue? (Yes/No)" << endl;
+
+			cin >> YesOrNo;
+
+			while (YesOrNo != "Yes" and YesOrNo != "No")
+			{
+				cin >> YesOrNo;
+			}
+
+			if (YesOrNo == "Yes")
+			{
+				system("pause");
+				system("cls");
+			}
+			else
+			{
+				exit(0);
+			}
+			break;
+
+		case 2:
+
+			secondTopic(symbolTopics, answers, answersTwo, answersThree);
+
+			cout << endl;
 
 			cout << "Do you want to continue? (Yes/No)" << endl;
 
